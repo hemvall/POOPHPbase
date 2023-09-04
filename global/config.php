11d56fee754c3d4e92ea3f2fc464ee0907fd2630
quesque <s.quesque@orange.fr>
2023-09-04 17:50:27 +0200

    mise en ligne

---
+++ global/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Identifiants pour la base de données. Nécessaires à PDO2.
+define('SQL_DSN','mysql:dbname=jeuarcade;host=localhost:3308');
+define('SQL_USERNAME', 'root');
+define('SQL_PASSWORD', '');
+define('SQL_OPTIONS', 'array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")');
+
+
---
+++ modeles/ArmeBDD.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+function donneArmeByIdV1($id) {
+    
+    $pdo = PDO2::getInstance();
+    
+    $requete = $pdo->prepare("...");
+    $bv1 = $requete->bindValue(...);
+    $executionok = $requete->execute();
+    $lArme = $requete->fetch();
+    if (is_array($lArme)){
+         return $lArme;
+    }
+    else {
+             
+          throw new Exception("aucune arme pour cet identifiant");
+                    
+    }
+    
+    
+}
+
+
+?>
